a853b511e8a95aecd2e448c02662129016f5e705
riceel527-hash <riceel527@gmail.com>
2026-09-23 08:05:44 -0400

    Initial commit

---
+++ src/MyComponent.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const MyComponent = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+
+ fetch('https://api.example.com/data')
+    .then(response => response.json())
+    .then(data => setData(data));},[]);
+
+    return <div>{JSON.stringify(data)}</div>;
+    };
+ 
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is my React component.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
+
+
